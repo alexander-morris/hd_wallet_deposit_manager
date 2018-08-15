6959c252d7e40471244597e7cf23149659ae70a6
Alex <alex@theblockchaininstitute.org>
2018-08-15 00:39:08 -0500

    working extended public key derivation

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ const HDKey = require('hdkey')
 const wallet = require('ethereumjs-wallet')
 const ethUtil = require('ethereumjs-util');
 const Web3 = require('web3')
-var _bitcoreMnemonic = require('bitcore-mnemonic');
+const base58 = require('base-58');
+const _bitcoreMnemonic = require('bitcore-mnemonic');
+const bs58check = require('bs58check')
 
 var _bitcoreMnemonic2 = _interopRequireDefault(_bitcoreMnemonic);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -18,25 +20,50 @@ const master_priv_key = new _bitcoreMnemonic2.default(seed_phrase).toHDPrivateKe
 
 var seed = HDKey.fromMasterSeed(new Buffer.from(master_priv_key, 'hex'))
 
+
 var master = {
 	'priv':seed.privateKey.toString('hex'),
-	'pub':seed.publicKey.toString('hex')
+	'pub':seed.publicKey.toString('hex'),
+	'pubx':seed.publicExtendedKey
 };
+
+console.log(seed.publicExtendedKey)
+
+// var exPubKey = bs58check.encode(seed.publicKey)
+
+// console.log(seed.publicKey.toString('hex'))
+// console.log(exPubKey)
+// console.log(bs58check.decode(exPubKey, 'hex'))
+
+var seed2 = HDKey.fromExtendedKey(master.pubx)
 
 console.log("\n", master, "\n");
 
-for (var i = 0; i < 5; i++) {
-	deriveChild(i);
+for (var i = 0; i < 1; i++) {
+	deriveChildPrivate(i);
+	deriveChildPublic(i);
 }
 
-function deriveChild (nonce) {
+function deriveChildPrivate (nonce) {
 
 	var ETH_PATH = 'm/44\'/60\'/0\'/0/' + nonce
 	var node = seed.derive(ETH_PATH)
 	var privateKey = node._privateKey.toString('hex')
 	var pubKey = ethUtil.privateToPublic(node._privateKey)
 	var address = ethUtil.publicToAddress(pubKey).toString('hex')
+	var chaddress = ethUtil.toChecksumAddress(address);
+	console.log("New Wallet Generated", "\nAt path: " + ETH_PATH, "\nPriv: " + privateKey, "\nAddr: " + address,  "\nchAddr: " + chaddress, "\n", "\n" )
 
-	console.log("New Wallet Generated", "\nAt path: " + ETH_PATH, "\nPriv: " + privateKey, "\nAddr: " + address, "\n", "\n" )
+}
+
+function deriveChildPublic (nonce) {
+
+	var ETH_PATH = 'm/44\'/60\'/0\'/0/' + nonce
+	var node = seed.derive(ETH_PATH)
+	var privateKey = node._privateKey.toString('hex')
+	var pubKey = ethUtil.privateToPublic(node._privateKey)
+	var address = ethUtil.publicToAddress(pubKey).toString('hex')
+	var chaddress = ethUtil.toChecksumAddress(address);
+	console.log("New Wallet Generated", "\nAt path: " + ETH_PATH, "\nPriv: " + privateKey, "\nAddr: " + address,  "\nchAddr: " + chaddress, "\n", "\n" )
 
 }
